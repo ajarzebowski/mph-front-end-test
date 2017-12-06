f81df9db16d33a7f5d391a3baf31761116039fed
Nikolay Nikolov <nikolay@michaelpaulholidays.co.uk>
2017-12-06 14:52:14 +0000

    batch file added - it have to be changed for different directory usage

---
+++ Front-end test.bat
@@ -0,0 +1,4 @@
+cd "C:\Front End Developer Technical Test"
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --new-window "http://localhost:3000/requirements.html" "http://localhost:3000"
+start node app.js
+start atom -n public/index.html
